54e113bd53ac1f435b22b2ab4aefa454f703228f
bpowell461 <bpowell461@gmail.com>
2023-01-15 12:16:26 -0600

    [development] UART Init and send data example (polling)

--- Src/main.c
+++ Src/main.c
@@ -1,15 +1,26 @@
 #include "main.h"
 #include "stm32f4xx_hal.h"
-
+#include <string.h>
 void SystemClockConfig(void);
 void UART2_Init(void);
+void Error_Handler(void);
 
-SPI_HandleTypeDef
+UART_HandleTypeDef huart2;
+
+static char *user_data = "Hello World\r\n";
 
 int main(void)
 {
     HAL_Init();
     SystemClockConfig();
+    UART2_Init();
+
+    if (HAL_OK != HAL_UART_Transmit(&huart2, (uint8_t*)user_data, strlen(user_data), HAL_MAX_DELAY))
+    {
+        Error_Handler();
+    }
+
+    while(1);
 
     return 0;
 }
@@ -21,5 +32,24 @@ void SystemClockConfig(void)
 
 void UART2_Init(void)
 {
+    // Linking address
+    huart2.Instance = USART2;
 
+    //Initialization
+    huart2.Init.BaudRate    = 115200;
+    huart2.Init.WordLength  = UART_WORDLENGTH_8B;
+    huart2.Init.StopBits    = UART_STOPBITS_1;
+    huart2.Init.Parity      = UART_PARITY_NONE;
+    huart2.Init.HwFlowCtl   = UART_HWCONTROL_NONE;
+    huart2.Init.Mode        = UART_MODE_TX_RX;
+    if( HAL_UART_Init(&huart2) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void Error_Handler(void)
+{
+    // Do something here
+    while (1);
 }
--- Src/stm32f4xx_hal_msp.c
+++ Src/stm32f4xx_hal_msp.c
@@ -14,3 +14,29 @@ void HAL_MspInit(void)
     HAL_NVIC_SetPriority(BusFault_IRQn, 0, 0);
     HAL_NVIC_SetPriority(UsageFault_IRQn, 0, 0);
 }
+
+void HAL_UART_MspInit(UART_HandleTypeDef *huart)
+{
+    // Enable peripheral clock
+    __HAL_RCC_USART2_CLK_ENABLE();
+
+    // Configure pin muxing
+    GPIO_InitTypeDef gpio_uart;
+
+    // Configuring TX Line
+    gpio_uart.Pin       = GPIO_PIN_2;
+    gpio_uart.Mode      = GPIO_MODE_AF_PP;
+    gpio_uart.Pull      = GPIO_PULLDOWN;
+    gpio_uart.Speed     = GPIO_SPEED_FREQ_LOW;
+    gpio_uart.Alternate = GPIO_AF7_USART2;
+
+    HAL_GPIO_Init(GPIOA, &gpio_uart);
+
+    // Configuring RX Line
+    gpio_uart.Pin = GPIO_PIN_3;
+    HAL_GPIO_Init(GPIOA, &gpio_uart);
+
+    // Enable IRQ and set up priority
+    HAL_NVIC_EnableIRQ(USART2_IRQn);
+    HAL_NVIC_SetPriority(USART2_IRQn, 15, 0);
+}
