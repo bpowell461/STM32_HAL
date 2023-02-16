49403c40dbaccf46eb196713f5528f06f9c562b0
bpowell461 <bpowell461@gmail.com>
2023-02-16 12:51:20 -0600

    Enabling CAN

---
+++ CAN_Example/Src/usb_host.c
@@ -0,0 +1,90 @@
+/* USER CODE BEGIN Header */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+
+#include "usb_host.h"
+#include "usbh_core.h"
+#include "usbh_cdc.h"
+
+/* USER CODE BEGIN Includes */
+/* USER CODE END Includes */
+
+/* USER CODE BEGIN PV */
+/* USER CODE END PV */
+
+/* USER CODE BEGIN PFP */
+/* USER CODE END PFP */
+
+/* USB Host core handle declaration */
+USBH_HandleTypeDef hUsbHostFS;
+ApplicationTypeDef Appli_state = APPLICATION_IDLE;
+
+/*
+ * -- Insert your variables declaration here --
+ */
+/* USER CODE BEGIN 0 */
+/* USER CODE END 0 */
+
+/*
+ * user callback declaration
+ */
+static void USBH_UserProcess(USBH_HandleTypeDef *phost, uint8_t id);
+
+/*
+ * -- Insert your external function declaration here --
+ */
+/* USER CODE BEGIN 1 */
+/* USER CODE END 1 */
+
+/**
+  * Init USB host library, add supported class and start the library
+  * @retval None
+  */
+void MX_USB_HOST_Init(void)
+{
+  /* USER CODE BEGIN USB_HOST_Init_PreTreatment */
+  /* USER CODE END USB_HOST_Init_PreTreatment */
+
+  /* Init host Library, add supported class and start the library. */
+  if (USBH_Init(&hUsbHostFS, USBH_UserProcess, HOST_FS) != USBH_OK)
+  {
+    Error_Handler();
+  }
+  if (USBH_RegisterClass(&hUsbHostFS, USBH_CDC_CLASS) != USBH_OK)
+  {
+    Error_Handler();
+  }
+  if (USBH_Start(&hUsbHostFS) != USBH_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USB_HOST_Init_PostTreatment */
+  /* USER CODE END USB_HOST_Init_PostTreatment */
+}
+
+/*
+ * Background task
+ */
+void MX_USB_HOST_Process(void)
+{
+  /* USB Host Background task */
+  USBH_Process(&hUsbHostFS);
+}
+/*
+ * user callback definition
+ */
+static void USBH_UserProcess  (USBH_HandleTypeDef *phost, uint8_t id)
+{
+  /* USER CODE BEGIN CALL_BACK_1 */
+  /* USER CODE END CALL_BACK_1 */
+}
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
